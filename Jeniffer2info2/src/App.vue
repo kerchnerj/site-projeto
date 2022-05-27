0dc83cf339230e6dc17424d186e566d4a41cc491
kerchnerj <jenifferkerchner12@gmail.com>
2022-05-27 09:02:04 -0300

    alteração no código

---
+++ Jeniffer2info2/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import BlocoPrincipalVue from './components/BlocoPrincipal.vue';
+
+</script>
+
+<template>
+  <header>
+    <div>
+   
+    </div>
+  </header>
+
+  <main>
+<ul>
+    <li><a href="#"> Home </a></li>
+    <li><a href="#"> Menu </a></li>
+    <li><a href="#">Curiosidades </a></li>
+</ul>
+
+  </main>
+  <footer>Rodapé</footer>
+</template>
+
+ <style>
+ @import"@/assets/style.css";
+</style>
+
---
+++ Jeniffer2info2/src/components/BlocoPrincipal.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <div>
+   
+    </div>
+  </header>
+
+  <main>
+<ul>
+    <li><a href="#"> Home </a></li>
+    <li><a href="#"> Menu </a></li>
+    <li><a href="#">Curiosidades </a></li>
+</ul>
+
+  </main>
+  <footer>Rodapé</footer>
+</template>
+
+ <style>
+
+</style>
